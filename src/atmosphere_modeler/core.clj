931b2f2bcd451209ad7f0d04e6167df83db7cdaf
danielmartincraig <danielmartincraig@gmail.com>
2019-12-07 21:40:29 -0600

    Defined initial important constants and struct and world defining setup function

--- src/atmosphere_modeler/core.clj
+++ src/atmosphere_modeler/core.clj
@@ -5,3 +5,23 @@
   "I don't do a whole lot ... yet."
   [& args]
   (println "Hello, World!"))
+
+; Define the important constants
+; Define the length of the world
+(def world-length 10)
+; Define the width of the world
+(def world-width 10)
+; Define a struct that represents a cube of air for purposes of the simulation
+(defstruct cube-of-air :air-pressure)
+
+(def myworld (ref (zone 0)))
+
+(def world
+  (apply vector
+         (map (fn [_]
+                (apply vector (map (fn [_] (ref (struct cube-of-air 0)))
+                                   (range world-width))))
+              (range world-length))))
+
+
+
